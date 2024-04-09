70ff194e2a04bcfab560931c5b5237c69dc44016
guy4shai <guyhazannnn@gmail.com>
2024-04-09 13:07:08 +0300

    Changes

--- test/Board.java
+++ test/Board.java
@@ -1,6 +1,54 @@
 package test;
-
+import java.util.ArrayList;
 
 public class Board {
 
+    private static Board instance = null;
+    private Tile[][] tiles;
+
+    private Board() {
+
+        tiles = new Tile[15][15];
+    }
+
+    public static Board getBoard() {
+
+        if (instance == null) {
+            instance = new Board();
+        }
+
+        return instance;
+    }
+
+    public Tile[][] getTiles() {
+
+        Tile[][] copy = new Tile[15][15];
+
+        for (int i = 0; i < 15; i++) {
+
+            for (int j = 0; j < 15; j++) {
+                copy[i][j] = tiles[i][j];
+            }
+            
+        }
+
+        return copy;
+    }
+
+    public boolean boardLegal(Word word) {
+        return true;
+    }
+
+    public boolean dictionaryLegal(Word word) {
+        return true;
+    }
+
+    public ArrayList<Word> getWords(Word word) {
+        return new ArrayList<>();
+    }
+
+    public int getScore(Word word) {
+        return 0;
+    }
 }
+
--- test/Word.java
+++ test/Word.java
@@ -1,7 +1,56 @@
 package test;
+import java.util.Arrays;
+
 
 
 public class Word {
 
-	
+    private Tile[] tiles;
+    private int row;
+    private int col;
+    private boolean vertical;
+
+    public Word(Tile[] tiles, int row, int col, boolean vertical) {
+
+        this.tiles = tiles;
+        this.row = row;
+        this.col = col;
+        this.vertical = vertical;
+    }
+
+    public Tile[] getTiles() {
+        return tiles;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isVertical() {
+        return vertical;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Word other = (Word) obj;
+
+        return row == other.row &&
+                col == other.col &&
+                vertical == other.vertical &&
+                Arrays.equals(tiles, other.tiles);
+    }
 }
+
